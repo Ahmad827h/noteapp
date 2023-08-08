2550644d23ab836f89d8c49a02764b05ca96d605
Ahmad827h <alzubyahmad3@gmail.com>
2023-08-08 11:31:57 +0300

    create Not APP

--- ap/lib/main.dart
+++ ap/lib/main.dart
@@ -12,38 +12,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-        create: (context) => CountCubit(),
-        child: GetMaterialApp(
-            home: SafeArea(
-          child: Scaffold(
-              body: Column(
-            children: [
-              BlocConsumer<CountCubit, CountState>(
-                listener: (context, state) {
-                  if (state == increatmtState(count: 1)) {
-                    print('yea');
-                  }
-                },
-                builder: (context, state) {
-                  return Container(
-                      child: Text(
-                    '${BlocProvider.of<CountCubit>(context).count}',
-                    style: TextStyle(fontSize: 40),
-                  ));
-                },
-              ),
-              BlocBuilder<CountCubit, CountState>(
-                builder: (context, state) {
-                  return ElevatedButton(
-                      onPressed: () {
-                        BlocProvider.of<CountCubit>(context).increamtFunCtion();
-                      },
-                      child: Text('add'));
-                },
-              )
-            ],
-          )),
-        )));
+    return Container();
   }
 }
